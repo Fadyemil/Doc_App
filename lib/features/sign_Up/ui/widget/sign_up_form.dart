dd3fcc5ad6637a770e5e66b75b1d99b7260b5b51
fady emil <fady46t45f@gmail.com>
2024-07-30 23:29:03 +0300

    add ui for sign up

---
+++ lib/features/sign_Up/ui/widget/sign_up_form.dart
@@ -0,0 +1,179 @@
+import 'package:doc_app/core/helpers/app_regex.dart';
+import 'package:doc_app/core/widget/app_text_form_field.dart';
+import 'package:doc_app/features/login/ui/widget/password_validations.dart';
+import 'package:doc_app/features/sign_Up/logic/cubit/sign_up_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../core/helpers/spacing.dart';
+
+class SignupForm extends StatefulWidget {
+  const SignupForm({super.key});
+
+  @override
+  State<SignupForm> createState() => _SignupFormState();
+}
+
+class _SignupFormState extends State<SignupForm> {
+  bool isPasswordObscureText = true;
+  bool isPasswordConfirmationObscureText = true;
+
+  bool hasLowercase = false;
+  bool hasUppercase = false;
+  bool hasSpecialCharacters = false;
+  bool hasNumber = false;
+  bool hasMinLength = false;
+
+  late TextEditingController passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    passwordController = context.read<SignupCubit>().passwordController;
+    setupPasswordControllerListener();
+  }
+
+  void setupPasswordControllerListener() {
+    passwordController.addListener(() {
+      setState(() {
+        hasLowercase = AppRegex.hasLowerCase(passwordController.text);
+        hasUppercase = AppRegex.hasUpperCase(passwordController.text);
+        hasSpecialCharacters =
+            AppRegex.hasSpecialCharacter(passwordController.text);
+        hasNumber = AppRegex.hasNumber(passwordController.text);
+        hasMinLength = AppRegex.hasMinLength(passwordController.text);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: context.read<SignupCubit>().formKey,
+      child: Column(
+        children: [
+          ///~ name
+          AppTextFormField(
+            keyboardType: TextInputType.name,
+            hintText: 'Name',
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a valid name';
+              }
+            },
+            controller: context.read<SignupCubit>().nameController,
+          ),
+          verticalSpace(18),
+
+          ///~phone
+          AppTextFormField(
+            keyboardType: TextInputType.phone,
+            hintText: 'Phone number',
+            validator: (value) {
+              if (value == null ||
+                  value.isEmpty ||
+                  !AppRegex.isPhoneNumberValid(value)) {
+                return 'Please enter a valid phone number';
+              }
+            },
+            controller: context.read<SignupCubit>().phoneController,
+          ),
+          verticalSpace(18),
+
+          ///~Email
+          AppTextFormField(
+            keyboardType: TextInputType.emailAddress,
+            hintText: 'Email',
+            validator: (value) {
+              if (value == null ||
+                  value.isEmpty ||
+                  !AppRegex.isEmailValid(value)) {
+                return 'Please enter a valid email';
+              }
+            },
+            controller: context.read<SignupCubit>().emailController,
+          ),
+          verticalSpace(18),
+
+          ///~Password
+          AppTextFormField(
+            controller: context.read<SignupCubit>().passwordController,
+            hintText: 'Password',
+            isObscureText: isPasswordObscureText,
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isPasswordObscureText = !isPasswordObscureText;
+                });
+              },
+              child: Icon(
+                isPasswordObscureText ? Icons.visibility_off : Icons.visibility,
+              ),
+            ),
+            validator: (value) {
+              if (value == null ||
+                  value.isEmpty ||
+                  !AppRegex.isPasswordValid(value)) {
+                return 'Please enter a valid password';
+              }
+            },
+          ),
+          verticalSpace(18),
+
+          ///~Password Confirmation
+          AppTextFormField(
+            keyboardType: TextInputType.visiblePassword,
+            controller:
+                context.read<SignupCubit>().passwordConfirmationController,
+            hintText: 'Password Confirmation',
+            isObscureText: isPasswordConfirmationObscureText,
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isPasswordConfirmationObscureText =
+                      !isPasswordConfirmationObscureText;
+                });
+              },
+              child: Icon(
+                isPasswordConfirmationObscureText
+                    ? Icons.visibility_off
+                    : Icons.visibility,
+              ),
+            ),
+            validator: (value) {
+              if (value == null ||
+                  value.isEmpty ||
+                  Password_Confirmation() == false) {
+                return 'Please enter a valid password';
+              }
+            },
+          ),
+          verticalSpace(24),
+          PasswordValidations(
+            hasLowerCase: hasLowercase,
+            hasUpperCase: hasUppercase,
+            hasSpecialCharacters: hasSpecialCharacters,
+            hasNumber: hasNumber,
+            hasMinLength: hasMinLength,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  bool Password_Confirmation() {
+    String pas = context.read<SignupCubit>().passwordController.text;
+    String pasCon =
+        context.read<SignupCubit>().passwordConfirmationController.text;
+    if (pas == pasCon) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
