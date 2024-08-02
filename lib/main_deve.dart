f1ae497c974b9f7fbfcf94fdb64b435ed3c7d0ee
fady emil <fady46t45f@gmail.com>
2024-08-02 11:53:04 +0300

    final fastFile

--- lib/main_deve.dart
+++ lib/main_deve.dart
@@ -1,10 +1,13 @@
 import 'package:doc_app/core/di/dependency_injection.dart';
+import 'package:doc_app/core/helpers/security.dart';
 import 'package:doc_app/core/routing/app_router.dart';
 import 'package:doc_app/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() async {
+Future<void> main() async {
+   await dotenv.load(fileName: Security.filename);
   // To fix texts being hidden bug in flutter_screenutil in release mode.
   await ScreenUtil.ensureScreenSize();
   setupGetIt();
