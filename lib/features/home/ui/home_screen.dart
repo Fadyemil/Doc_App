d4fd08f2b0403ef8c56b9510afa7316fa346efa2
fady emil <fady46t45f@gmail.com>
2024-08-03 23:07:39 +0300

    fix big in Ui

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:doc_app/features/home/ui/widget/doctors_speciality_see_all.dart'
 import 'package:doc_app/features/home/ui/widget/hom_top_bar.dart';
 import 'package:doc_app/features/home/ui/widget/specializations_and_doctors_bloc_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,20 +15,25 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          margin: const EdgeInsets.fromLTRB(20, 16, 20, 28),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              HomTopBar(),
-              verticalSpace(16),
-              DoctorsBlueContainer(),
-              verticalSpace(24),
-              DoctorsSpecialitySeeAll(),
-              verticalSpace(16),
-              SpecializationsAndDoctorsBlocBuilder(),
-            ],
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            margin: const EdgeInsets.fromLTRB(20, 16, 20, 28),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HomTopBar(),
+                verticalSpace(16),
+                DoctorsBlueContainer(),
+                verticalSpace(24),
+                DoctorsSpecialitySeeAll(),
+                verticalSpace(16),
+                Container(
+                  height: 400.h,
+                  child: SpecializationsAndDoctorsBlocBuilder(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
