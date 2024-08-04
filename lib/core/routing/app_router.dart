c3882fb5eb8dc5e1dce17ce15c163e889e75fe19
fady emil <fady46t45f@gmail.com>
2024-08-04 15:07:23 +0300

    current name page

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,38 +1,37 @@
 // ignore_for_file: unused_local_variable
 
-import 'package:doc_app/core/di/dependency_injection.dart';
+import 'package:flutter/material.dart';
 import 'package:doc_app/core/routing/routes.dart';
 import 'package:doc_app/features/home/logic/cubit/home_cubit.dart';
 import 'package:doc_app/features/home/ui/home_screen.dart';
 import 'package:doc_app/features/login/logic/cubit/login_cubit.dart';
-import 'package:doc_app/features/login/ui/Login_screen.dart';
+import 'package:doc_app/features/login/ui/login_screen.dart';
 import 'package:doc_app/features/onboarding/ui/onboarding_screen.dart';
 import 'package:doc_app/features/sign_Up/logic/cubit/sign_up_cubit.dart';
 import 'package:doc_app/features/sign_Up/ui/sign_up_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:doc_app/core/di/dependency_injection.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
-    //this arguments to be passed in any screen like this ( arguments as ClassName )
     final arguments = settings.arguments;
     switch (settings.name) {
       case Routes.homeScreen:
         return funHomeScreen();
       case Routes.loginScreen:
-        return funLoginScrren();
+        return funLoginScreen();
       case Routes.onBoardingScreen:
         return MaterialPageRoute(
             builder: (context) => const OnboardingScreen());
       case Routes.signUpScreen:
         return funSignUpScreen();
       default:
-        return funDefauitscreen(settings);
+        return funDefaultScreen(settings);
     }
   }
 
   MaterialPageRoute<dynamic> funHomeScreen() {
-     return MaterialPageRoute(
+    return MaterialPageRoute(
       builder: (context) => BlocProvider(
         create: (context) => HomeCubit(getIt())..getSpecializations(),
         child: const HomeScreen(),
@@ -40,17 +39,17 @@ class AppRouter {
     );
   }
 
-  MaterialPageRoute<dynamic> funDefauitscreen(RouteSettings settings) {
+  MaterialPageRoute<dynamic> funDefaultScreen(RouteSettings settings) {
     return MaterialPageRoute(
       builder: (context) => Scaffold(
         body: Center(
-          child: Text(' No route found for this page ${settings.name}'),
+          child: Text('No route found for this page ${settings.name}'),
         ),
       ),
     );
   }
 
-  MaterialPageRoute<dynamic> funLoginScrren() {
+  MaterialPageRoute<dynamic> funLoginScreen() {
     return MaterialPageRoute(
       builder: (context) => BlocProvider(
         create: (context) => getIt<LoginCubit>(),
